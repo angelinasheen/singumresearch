87c0f2059b51705e7c92e0489be1d64ea7a82bde
SnoopyCodes <97713249+SnoopyCodes@users.noreply.github.com>
2025-07-02 13:51:35 -0500

    plot change 2 lines idk how to unstage this

--- plotter.py
+++ plotter.py
@@ -2,11 +2,15 @@ from shapely.geometry import Polygon, MultiPolygon
 import geopandas as gpd
 import matplotlib.pyplot as plt
 
-gdf = gpd.read_file("manhattan_boundaries.geojson")
+manhattan_gdf = gpd.read_file("manhattan_boundaries.geojson")
 
 fig, ax = plt.subplots()
 
-gdf.plot(ax=ax, facecolor=None, edgecolor=None, linewidth=1)
+manhattan_gdf.plot(ax=ax, facecolor=None, edgecolor=None, linewidth=1)
+
+# just plot each singum i guess
+singum_gdf = None
+# singum_gdf.plot(ax=ax, facecolor="black", edgecolor="red", linewidth=1)
 
 plt.title("visualize singums")
 plt.show()
